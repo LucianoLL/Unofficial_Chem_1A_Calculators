6ff9a124b5b374970bf238285d6aaf89bab6ed5e
LucianoLL <56811002+LucianoLL@users.noreply.github.com>
2022-05-11 16:21:44 -0700

    Starting to declare the functions needs to construct the dicitionary for the periodic table

--- MolarMassClient.cpp
+++ MolarMassClient.cpp
@@ -1,10 +1,11 @@
-/* Molar Mass client claculator
+/* Molar Mass client calculator
  * Luciano Loma Lorenzana
  * Develop: 05/11/2022
  */
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include "MolarMass.h"
 
 int main (int argc, char *argv[]) {
@@ -25,4 +26,15 @@ int main (int argc, char *argv[]) {
     }
 
     compoundAmounts = std::stoi(failSafe);  //  converting the users input into an actual string
+
+    std::string currCompound;  //  a buffer to store the users current compound
+    std::vector<std::string> compoundList;  //  to store all of the user's compound inputs
+
+
+    for (int i = 0; i < compoundAmounts; i++) {
+        std::cout << "Type comound " << i << " of " << compoundAmounts << " : " << "\n";
+        std::cin >> currCompound;  //  taking in the user's inputs
+        std::cout << "\n";
+        compoundList.push_back(currCompound);  //  adding them into the vector
+    }
 }
---
+++ PeriodicTableDicitonary.h
@@ -0,0 +1,12 @@
+/* Periodic Table dicitonary header file
+ * Luciano Loma Lorenzana
+ * Develop: 05/11/2022
+ */
+
+#include <iostream>
+#include <string>
+
+#ifndef PERIODIC_TABLE_H_
+#define PERIODIC_TABLE_H_
+
+#endif
