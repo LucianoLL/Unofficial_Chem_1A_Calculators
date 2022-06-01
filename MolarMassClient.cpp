9cd8798be5d13d19c8a693ccd0b14bb8bff03dc9
LucianoLL <56811002+LucianoLL@users.noreply.github.com>
2022-06-01 15:30:31 -0700

    Editted the client file

--- MolarMassClient.cpp
+++ MolarMassClient.cpp
@@ -15,38 +15,30 @@ int main () {
     PeriodicTable Table;  //  declaring the table
     Table.InitTable();  //  initializes/builds the periodic table
 
-    int compoundAmounts;  //  what will eventually become a digit
-    std::string failSafe;  //  passing argument as a string as a fail safe
-    std::cout << "Please type in the number of compounds you want calculated : " << "\n";
-    std::cin >> failSafe;
-    std::cout << "\n";
-    int stringLen = failSafe.length();  //  grabbing the length of the string input
+    std::string userComp = "";
+    bool fcnBool = true;
 
-    for (int i = 0; i < stringLen; i++) {
-        if (!isdigit(failSafe[i])) {
-            std::cout << "Please type an actual NUMBER of compounds you want calculated: " << "\n";
-            std::cin >> failSafe;
-            stringLen = failSafe.length();
-            i = 0;
+    std::cout << "Welcome to the Molar Mass Calculator\n"
+    << "You can exit anytime by typing the word exit \n"
+    << "Please type any compound : ";
+
+    std::cin >> userComp;
+    std::string userOpt = "";
+
+    while (fcnBool == true) {
+        if (userComp == "exit") {
+            fcnBool = false;
+        } else {
+            float mass = multElement(userComp, Table);
+            std::cout << "The Molar Mass of " << userComp << " is : " << mass << "\n";
         }
+        std::cout << "Would you like to try another compound : Yes or No?\n";
+        std::cin >> userOpt;
+        if ((userOpt == "No") || (userOpt == "no") || (userOpt == "exit")) {
+            fcnBool = false;
+        } else {
+            fcnBool = true;
+        }       
     }
-
-    compoundAmounts = std::stoi(failSafe);  //  converting the users input into an actual string
-
-    std::string currCompound;  //  a buffer to store the users current compound
-    std::vector<std::string> compoundList;  //  to store all of the user's compound inputs
-
-
-    for (int i = 0; i < compoundAmounts; i++) {
-        std::cout << "Type compound " << i + 1 << " of " << compoundAmounts << " : " << "\n";
-        std::cin >> currCompound;  //  taking in the user's inputs
-        std::cout << "\n";
-        compoundList.push_back(currCompound);  //  adding them into the vector
-    }
-
-    int listSize = compoundList.size();  //  to easily parse in through the vector
-    for (int i = 0; i < listSize; i++) {  //  going through the inputs
-        float molarMass = multElement(compoundList[i], Table);
-        std::cout << "Molar Mass of " << compoundList[i] << " is : " << molarMass << "\n";
-    }
+    exit(EXIT_SUCCESS);
 }
