d3bff16f3c0043b2422fe728d616ae46cb27953d
root <root@LucianoLaptop>
2024-08-29 14:58:44 -0700

    Writing the first function for phConversion.cpp

---
+++ code/phConversion.cpp
@@ -0,0 +1,13 @@
+/*
+ * pH Conversion Functions/ADT
+ * Luciano Loma Lorenzana
+ * Develop: 08/24/2024
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "phConversion.h"
+
+std::string 
